dbc8e22ac24a0af555de40e72b2fb4c349432caa
abdallahmohamedhassan <abdallah.hassan.eg@gmail.com>
2025-05-15 03:29:53 +0300

    category pages

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
         public IActionResult Index()
+        {List<Category> categories=_categoryRepository.GetAll().ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {if(obj.Name==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "this DisplayOrder Cannot exactly match the Name .");
+
+            }
+            if (ModelState.IsValid)
+            {_categoryRepository.Add(obj);
+                _categoryRepository.Save();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {if (id == null|| id==0) { return NotFound(); }
+        Category? category=_categoryRepository.Get(u=>u.Id==id);
+            if (category==null) return NotFound();
+            return View(category);
+        }
+        [HttpPost]
+
+        public IActionResult Edit(Category obj)
+        {if(ModelState.IsValid)
+            {
+                _categoryRepository.Update(obj);
+                _categoryRepository.Save();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+           
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) { return NotFound(); }
+            Category? category = _categoryRepository.Get(u => u.Id == id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? category = _categoryRepository.Get(u => u.Id == id);
+            if (category == null) return NotFound();
+            _categoryRepository.Remove(category);
+            _categoryRepository.Save();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+
+
+           
+
         }
     }
 }
