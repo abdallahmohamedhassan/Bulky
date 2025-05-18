9c1f673f5b4baa02e52f02cac34fbde2544687ec
abdallahmohamedhassan <abdallah.hassan.eg@gmail.com>
2025-05-18 04:04:58 +0300

    image

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -9,9 +9,11 @@ namespace BulkyWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -47,7 +49,17 @@ namespace BulkyWeb.Areas.Admin.Controllers
         {
            
             if (ModelState.IsValid)
-            {
+            {string wwwRootPath= _webHostEnvironment.WebRootPath;
+                if(file!=null)
+                {
+                    string fileName=Guid.NewGuid().ToString()+Path.GetExtension(file.FileName);
+                    string productPath=Path.Combine(wwwRootPath,@"images\product");
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    obj.Product.ImageUrl = @"\images\product\" + fileName;
+                }
                 _unitOfWork.productRepository.Add(obj.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product Created Successfully";
