bb0f5ba8dee5bbae4d0107010f898c018bea6277
abdallahmohamedhassan <abdallah.hassan.eg@gmail.com>
2025-07-07 01:27:22 +0300

    make the route work

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
