ec5c7b44e98703bf24f61385819ddd355967a8f9
abdallahmohamedhassan <abdallah.hassan.eg@gmail.com>
2025-07-07 17:01:54 +0300

    Authorization

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options=>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<IdentityUser,IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders() ;
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+
+});
 builder.Services.AddRazorPages(); // Optional but good for clarity
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
